fd7df958a15751d77e0490e66cc5f617e48e6cef
tpluscode <tpluscode@users.noreply.github.com>
2021-05-01 19:39:05 +0200

    build(deps): update shacl middleware

--- packages/knossos/test/shacl.test.ts
+++ packages/knossos/test/shacl.test.ts
@@ -3,7 +3,7 @@ import express from 'express'
 import request from 'supertest'
 import { turtle } from '@tpluscode/rdf-string'
 import TermSet from '@rdfjs/term-set'
-import { foaf, rdf, rdfs, sh } from '@tpluscode/rdf-ns-builders'
+import { foaf } from '@tpluscode/rdf-ns-builders'
 import sinon from 'sinon'
 import DatasetExt from 'rdf-ext/lib/Dataset'
 import $rdf from 'rdf-ext'
@@ -46,33 +46,6 @@ describe('@hydrofoil/knossos/shacl', () => {
 
   afterEach(() => {
     sinon.restore()
-  })
-
-  it.skip('validates resource using hierarchy of shapes', async () => {
-    // given
-    shapesGraph.node(foaf.Person)
-      .addOut(rdfs.subClassOf, foaf.Agent)
-      .addOut(rdf.type, [rdfs.Class, sh.NodeShape])
-      .node(foaf.Agent)
-      .addOut(rdf.type, [rdfs.Class, sh.NodeShape])
-      .addOut(sh.property, property => {
-        property
-          .addOut(sh.path, foaf.name)
-          .addOut(sh.minCount, 1)
-      })
-    app.post('*', shaclValidate, ok)
-
-    // when
-    const response = request(app)
-      .post('/')
-      .send(turtle`<> a ${foaf.Person} .`.toString())
-      .set('content-type', 'text/turtle')
-
-    // then
-    await response.expect(400)
-      .expect(res => {
-        expect(res.text).to.contain('MinCount')
-      })
   })
 
   it('loads shapes for the sum of resource types and payload types', async () => {
