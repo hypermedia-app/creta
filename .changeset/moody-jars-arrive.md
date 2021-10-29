7276f13ec7fb7fc439ac23d122376536a8279b2e
tpluscode <tpluscode@users.noreply.github.com>
2021-10-29 18:47:12 +0200

    feat: allow optional ACL rules

---
+++ .changeset/moody-jars-arrive.md
@@ -0,0 +1,5 @@
+---
+"@hydrofoil/knossos": patch
+---
+
+All providing additional authorization patterns to `rdf-web-access-control`
--- packages/knossos/lib/settings.ts
+++ packages/knossos/lib/settings.ts
@@ -12,6 +12,7 @@ import toArray from 'stream-to-array'
 import { SELECT } from '@tpluscode/sparql-builder'
 import { knossos, code } from '@hydrofoil/vocabularies/builders/strict'
 import { hydra, schema } from '@tpluscode/rdf-ns-builders/strict'
+import { AuthorizationPatterns } from 'rdf-web-access-control'
 import type { Context } from '../server'
 
 /**
@@ -62,11 +63,48 @@ export async function loadMiddlewares(
 
     const factory = await loadCode<MiddlewareFactory>(link)
     if (!factory) {
-      log(`Failed to load middleware ${name} from ${link.out(code.link).value}`)
+      log(`Failed to load ${name} middleware from ${link.out(code.link).value}`)
     } else {
+      log(`Loaded ${name} middleware from ${link.out(code.link).value}`)
       map[name].push(await factory(context))
     }
 
     return map
   }, Promise.resolve<Record<string, express.RequestHandler[]>>({}))
+}
+
+export async function loadAuthorizationPatterns(
+  api: Api,
+  log: Debugger,
+  context: Context,
+  { getConfigurationId = defaultConfigurationQuery }: { getConfigurationId?: typeof defaultConfigurationQuery } = {},
+): Promise<AuthorizationPatterns[]> {
+  const loadCode = codeLoader(api)
+
+  const configUri = await getConfigurationId(api, context)
+
+  if (!configUri) {
+    log('No configuration resource found')
+    return []
+  }
+
+  const config = await context.store.load(configUri)
+  const authorizationPattern = config.out(knossos.authorizationRule).toArray()
+
+  return authorizationPattern.reduce(async (previous, next) => {
+    const arr = await previous
+    const [link] = next.out(code.implementedBy).toArray()
+
+    const patternFactory = await loadCode<AuthorizationPatterns>(link)
+    if (!patternFactory) {
+      log(`Failed to load ${link.out(code.link).value}`)
+      return arr
+    }
+
+    log(`Loaded authorization rule ${link.out(code.link).value}`)
+    return [
+      ...arr,
+      patternFactory,
+    ]
+  }, Promise.resolve<AuthorizationPatterns[]>([]))
 }
