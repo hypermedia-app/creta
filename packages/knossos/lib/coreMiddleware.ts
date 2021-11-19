8191e416c1510f83377248e773955766ded224a5
tpluscode <tpluscode@users.noreply.github.com>
2021-11-19 18:15:22 +0100

    feat: running API on subpath

--- packages/knossos/lib/coreMiddleware.ts
+++ packages/knossos/lib/coreMiddleware.ts
@@ -3,6 +3,7 @@ import $rdf from 'rdf-ext'
 import express from 'express'
 import type StreamClient from 'sparql-http-client/StreamClient'
 import { Debugger } from 'debug'
+import absoluteUrl from 'absolute-url'
 import { createHydraBox } from './middleware/hydraBox'
 import { ResourceStore } from './store'
 
@@ -16,16 +17,20 @@ interface Options {
   log: Debugger
 }
 
-export const coreMiddleware = ({ name, path, codePath, client, sparql, store, log }: Options): express.RequestHandler => {
+export const coreMiddleware = ({ name, path, codePath, client, sparql, store, log }: Options, createApi = createHydraBox): express.RequestHandler => {
   const apisMiddlewares = new Map()
 
   return async (req, res, next) => {
-    let hydraMiddleware = apisMiddlewares.get(req.hostname)
+    absoluteUrl.attach(req)
+
+    const apiKey = req.hostname + req.baseUrl
+
+    let hydraMiddleware = apisMiddlewares.get(apiKey)
     if (!hydraMiddleware) {
       const iri = new URL(req.absoluteUrl())
       const apiIri = new URL(join(req.baseUrl, path), iri)
 
-      hydraMiddleware = await createHydraBox({
+      hydraMiddleware = await createApi({
         apiTerm: $rdf.namedNode(apiIri.toString()),
         sparql,
         client,
@@ -36,7 +41,7 @@ export const coreMiddleware = ({ name, path, codePath, client, sparql, store, lo
         path,
         log,
       })
-      apisMiddlewares.set(req.hostname, hydraMiddleware)
+      apisMiddlewares.set(apiKey, hydraMiddleware)
     }
 
     hydraMiddleware(req, res, next)
---
+++ packages/knossos/test/lib/coreMiddleware.test.ts
@@ -0,0 +1,54 @@
+import { expect } from 'chai'
+import express from 'express'
+import sinon from 'sinon'
+import request from 'supertest'
+import $rdf from 'rdf-ext'
+import { coreMiddleware } from '../../lib/coreMiddleware'
+
+describe('@hydrofoil/knossos/lib/coreMiddleware', () => {
+  const options = {
+    name: 'test-api',
+    path: '/api',
+    codePath: 'lib',
+    apiRoots: [],
+    client: {} as any,
+    sparql: {} as any,
+    store: {} as any,
+    log: sinon.spy() as any,
+  }
+
+  let app: express.Express
+  let createMockApi: sinon.SinonStub
+
+  beforeEach(() => {
+    app = express()
+    createMockApi = sinon.stub<any[], Promise<express.RequestHandler>>().resolves((req, res) => res.end())
+  })
+
+  it('creates api on subpath', async () => {
+    // given
+    app.use(/\/(people|projects)/, coreMiddleware({
+      ...options,
+    }, createMockApi))
+
+    // when
+    await request(app)
+      .get('/people/john-doe')
+      .set('Host', 'example.com')
+
+    // then
+    expect(createMockApi).to.have.been.calledWithMatch({
+      apiTerm: $rdf.namedNode('http://example.com/people/api'),
+    })
+
+    // when
+    await request(app)
+      .get('/projects/shaperone')
+      .set('Host', 'example.com')
+
+    // then
+    expect(createMockApi).to.have.been.calledWithMatch({
+      apiTerm: $rdf.namedNode('http://example.com/projects/api'),
+    })
+  })
+})
