ebfccd1f2825c2384627875fc55ec40ceeb29e58
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-12-02 14:08:06 +0100

    feat: complex query:include

--- packages/labyrinth/lib/query/eagerLinks.ts
+++ packages/labyrinth/lib/query/eagerLinks.ts
@@ -1,41 +1,22 @@
-import { NamedNode } from 'rdf-js'
 import { GraphPointer, MultiPointer } from 'clownface'
-import { SparqlGraphQueryExecutable } from '@tpluscode/sparql-builder/lib'
 import { DESCRIBE } from '@tpluscode/sparql-builder'
 import DatasetExt from 'rdf-ext/lib/Dataset'
 import $rdf from 'rdf-ext'
 import type StreamClient from 'sparql-http-client/StreamClient'
 import TermSet from '@rdfjs/term-set'
-
-function getLinkedResources(resource: MultiPointer, property: NamedNode): SparqlGraphQueryExecutable | null {
-  const linked = resource.out(property)
-
-  if (linked.values.length === 0) {
-    return null
-  }
-
-  const linkedUris = [...linked.terms].filter(({ termType }) => termType === 'NamedNode')
-  return DESCRIBE`${linkedUris}`
-}
+import { findNodes } from 'clownface-shacl-path'
 
 export async function loadLinkedResources(resource: MultiPointer, links: GraphPointer[], sparql: StreamClient): Promise<DatasetExt> {
   const dataset = $rdf.dataset()
 
-  const uniqueLinks = new TermSet(links.map(link => link.term))
+  const linked = new TermSet(links
+    .flatMap(link => findNodes(resource, link).terms)
+    .filter(term => term.termType === 'NamedNode'))
 
-  await Promise.all(([...uniqueLinks].reduce((promises, term) => {
-    if (term.termType === 'NamedNode') {
-      promises.push((async () => {
-        const query = getLinkedResources(resource, term)
-
-        if (query) {
-          const stream = await query.execute(sparql.query)
-          await dataset.import(stream)
-        }
-      })())
-    }
-    return promises
-  }, [] as Promise<void>[])))
+  if (linked.size) {
+    const stream = await DESCRIBE`${linked}`.execute(sparql.query)
+    await dataset.import(stream)
+  }
 
   return dataset
 }
