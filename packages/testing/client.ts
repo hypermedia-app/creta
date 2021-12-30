daa0fde9205c5eb09a625655f0a478039bf1eded
tpluscode <tpluscode@users.noreply.github.com>
2021-12-30 12:10:00 +0100

    fix(shacl): load nested shapes

--- packages/testing/client.ts
+++ packages/testing/client.ts
@@ -1,6 +1,7 @@
 import StreamClient from 'sparql-http-client/StreamClient'
-import { sparql, SparqlTemplateResult } from '@tpluscode/rdf-string'
+import { sparql } from '@tpluscode/rdf-string'
 import { DELETE, INSERT } from '@tpluscode/sparql-builder'
+import { SparqlValue } from '@tpluscode/rdf-string/lib/sparql'
 
 export const client = new StreamClient({
   endpointUrl: 'http://db.labyrinth.lndo.site/repositories/tests?infer=true',
@@ -9,7 +10,9 @@ export const client = new StreamClient({
   password: 'password',
 })
 
-export function testData(dataset: SparqlTemplateResult): Promise<void> {
+export function testData(strings: TemplateStringsArray, ...values: SparqlValue[]): Promise<void> {
+  const dataset = sparql(strings, ...values)
+
   const del = DELETE`?s ?p ?o`.WHERE`?s ?p ?o`
   const insert = INSERT.DATA`${dataset}`
 
