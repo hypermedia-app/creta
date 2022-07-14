8bb9f2cf81febbc9848f6a84ed9558b813e2f90e
tpluscode <tpluscode@users.noreply.github.com>
2022-07-14 21:52:16 +0200

    fix: Filter generic arg

---
+++ .changeset/friendly-swans-tap.md
@@ -0,0 +1,5 @@
+---
+"@hydrofoil/labyrinth": patch
+---
+
+`Filter` should be generic
--- packages/labyrinth/lib/query/filters.ts
+++ packages/labyrinth/lib/query/filters.ts
@@ -7,7 +7,7 @@ import { sparql } from '@tpluscode/rdf-string'
 import { warn } from '../logger'
 import { ToSparqlPatterns } from '.'
 
-export type Filter = ToSparqlPatterns
+export type Filter<T extends unknown[] = []> = ToSparqlPatterns<T>
 
 export const exactMatch: Filter = ({ subject, predicate, object }) => {
   const [term, ...more] = object.terms
