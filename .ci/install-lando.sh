981347b27e85d1d1e27d5526dcefeb5760ea7435
tpluscode <tpluscode@users.noreply.github.com>
2022-08-21 15:52:27 +0200

    ci: lando action

--- .ci/install-lando.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-curl -fsSL -o /tmp/lando-latest.deb https://github.com/lando/lando/releases/download/v3.0.26/lando-v3.0.26.deb
-sudo dpkg -i /tmp/lando-latest.deb
-lando version
