d537111ac7f71a9219a6534cda366d4bfc761e7d
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-11-01 12:28:27 +0100

    chore: remove unneeded param

--- packages/shape-to-query/lib/shapeToQuery.ts
+++ packages/shape-to-query/lib/shapeToQuery.ts
@@ -2,7 +2,7 @@ import type { GraphPointer } from 'clownface'
 import { CONSTRUCT } from '@tpluscode/sparql-builder'
 import { shapeToPatterns, Options } from './shapeToPatterns'
 
-export function construct(shape: GraphPointer, options: Options = {}) {
+export function construct(shape: GraphPointer, options: Omit<Options, 'strict'> = {}) {
   return CONSTRUCT`
     ${shapeToPatterns(shape, { ...options, strict: true })}
   `.WHERE`
