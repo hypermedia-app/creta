7eb7ae4e2ae26a83c1e826e447b5316182393cd2
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-01-10 14:21:31 +0100

    feat: delete query

--- packages/shape-to-query/test/index.test.ts
+++ packages/shape-to-query/test/index.test.ts
@@ -424,4 +424,155 @@ describe('@hydrofoil/shape-to-query', () => {
       })
     })
   })
+
+  context('shapes with complex paths and sh:node', () => {
+    context('sh:zeroOrMorePath', () => {
+      it('generates a deep pattern', async () => {
+        // given
+        const shape = await parse`
+          <>
+            a ${sh.NodeShape} ;
+            ${sh.property}
+            [
+              ${sh.path} [ ${sh.zeroOrMorePath} ${foaf.knows} ] ;
+              ${sh.node}
+              [
+                ${sh.property}
+                [
+                  ${sh.path} ${foaf.name} ;
+                ] ;
+              ] ;
+            ] ; 
+          .
+        `
+
+        // when
+        const patterns = shapeToPatterns(shape, { subjectVariable: 'node' })
+        const query = SELECT.ALL.WHERE`${patterns.whereClause()}`.build()
+
+        // then
+        expect(query).to.be.a.query(sparql`
+          SELECT * WHERE {
+            {
+              ?node ${foaf.knows}* ?node_0_i .
+              ?node_0_i ${foaf.knows} ?node_0 .
+            }
+            UNION
+            {
+              ?node ${foaf.knows}* ?node_0_i .
+              ?node_0_i ${foaf.knows} ?node_0 .
+              ?node_0 ${foaf.name} ?node_0_0 .
+            }
+          }
+        `)
+      })
+
+      it('produces correct CONSTRUCT clause', async () => {
+        // given
+        const shape = await parse`
+        <>
+          ${sh.property}
+            [
+              ${sh.path} [ ${sh.zeroOrMorePath} ${ex.nextInHierarchy} ] ;
+              ${sh.node}
+                [
+                  ${sh.property}
+                    [
+                      ${sh.path} ${schema.name} ;
+                    ],
+                    [
+                      ${sh.path} ${sh.path} ;
+                      ${sh.node}
+                        [
+                          ${sh.property}
+                            [
+                              ${sh.path} ${sh.inversePath} ;
+                            ] ;
+                        ] ;
+                    ] ;
+                ] ;
+            ] .
+        `
+
+        // when
+        const query = constructQuery(shape, { subjectVariable: 'node' })
+
+        // then
+        expect(query).to.be.a.query(sparql`
+          CONSTRUCT {
+            ?node_0_i ${ex.nextInHierarchy} ?node_0 .
+            ?node_0 ${schema.name} ?node_0_0 .
+            ?node_0 ${sh.path} ?node_0_1 .
+            ?node_0_1 ${sh.inversePath} ?node_0_1_0 .
+          } WHERE {
+            {
+              ?node ${ex.nextInHierarchy}* ?node_0_i .
+              ?node_0_i ${ex.nextInHierarchy} ?node_0 .
+            }
+            UNION 
+            {
+              ?node ${ex.nextInHierarchy}* ?node_0_i .
+              ?node_0_i ${ex.nextInHierarchy} ?node_0 .
+              ?node_0 ${schema.name} ?node_0_0 .
+            }
+            UNION 
+            {
+              ?node ${ex.nextInHierarchy}* ?node_0_i .
+              ?node_0_i ${ex.nextInHierarchy} ?node_0 .
+              ?node_0 ${sh.path} ?node_0_1 .
+            }
+            UNION 
+            {
+              ?node ${ex.nextInHierarchy}* ?node_0_i .
+              ?node_0_i ${ex.nextInHierarchy} ?node_0 .
+              ?node_0 ${sh.path} ?node_0_1 .
+              ?node_0_1 ${sh.inversePath} ?node_0_1_0 .
+            }
+          }
+        `)
+      })
+    })
+
+    context('sh:oneOrMorePath', () => {
+      it('generates a deep pattern', async () => {
+        // given
+        const shape = await parse`
+          <>
+            a ${sh.NodeShape} ;
+            ${sh.property}
+            [
+              ${sh.path} [ ${sh.oneOrMorePath} ${foaf.knows} ] ;
+              ${sh.node}
+              [
+                ${sh.property}
+                [
+                  ${sh.path} ${foaf.name} ;
+                ] ;
+              ] ;
+            ] ; 
+          .
+        `
+
+        // when
+        const patterns = shapeToPatterns(shape, { subjectVariable: 'node' })
+        const query = SELECT.ALL.WHERE`${patterns.whereClause()}`.build()
+
+        // then
+        expect(query).to.be.a.query(sparql`
+          SELECT * WHERE {
+            {
+              ?node ${foaf.knows}* ?node_0_i .
+              ?node_0_i ${foaf.knows} ?node_0 .
+            }
+            UNION
+            {
+              ?node ${foaf.knows}* ?node_0_i .
+              ?node_0_i ${foaf.knows} ?node_0 .
+              ?node_0 ${foaf.name} ?node_0_0 .
+            }
+          }
+        `)
+      })
+    })
+  })
 })
